158a9361ea586b382e4bb545a4c11545779bb490
John Daniels <john@semantici.st>
2014-01-14 09:16:31 +0000

    Basic refinery install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HotDealNews::Application.config.secret_token = 'c8a04c72c81f3db329c5f0d06bb67f12f538d8e598e953c221ea6c628df8ba62eb08d8a375107eacc7902cfe145c7f8de393861d609bcfd0f61d9954f7a75f6b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+HotDealNews::Application.config.session_store :cookie_store, key: '_hot_deal_news_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# HotDealNews::Application.config.session_store :active_record_store
